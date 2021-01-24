adeb94d17ababb5b8ddf4ff097844cd8ebbcc758
deepakraj5 <deepakatr4@gmail.com>
2021-01-24 20:58:51 +0530

    expenses-tracker-starter

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/app.scss'
+import MainComponent from './MainComponent'
+
+const App = () => {
+  return (
+    <div>
+
+      <MainComponent />
+
+    </div>
+  );
+}
+
+export default App
---
+++ client/src/components/MainComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/app.scss'
+
+const MainComponent = () => {
+  return (
+    <div className="container">
+        
+        <div className="center-rec">
+
+        </div>
+
+    </div>
+  );
+}
+
+export default MainComponent
